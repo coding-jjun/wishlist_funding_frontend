32c947951898ce82f959e51b8baba864218fa8bb
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2024-04-12 21:07:54 +0900

    :sparkles: Feat: Material UI `ThemeProvider` 추가

---
+++ src/components/theme/palette.ts
@@ -0,0 +1,14 @@
+import { PaletteOptions } from "@mui/material";
+import { red } from "@mui/material/colors";
+
+const palette: PaletteOptions = {
+  primary: {
+    main: red[300],
+    contrastText: "#FFFFFF",
+  },
+  secondary: {
+    main: "#FFC2C7",
+  },
+};
+
+export default palette;
