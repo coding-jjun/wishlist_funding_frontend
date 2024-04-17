78d4c9351e4b3d532580d4c6fb494037fedf197f
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2024-04-17 16:41:42 +0900

    :sparkles: Feat: 펀딩 상세 정보 조회 페이지 및 정보 탭 추가

--- src/app/(without-navbar)/fundings/[fundId]/page.tsx
+++ src/app/(without-navbar)/fundings/[fundId]/page.tsx
@@ -1,7 +1,30 @@
-export default function FundingDetailPage() {
+"use client";
+import { Stack } from "@mui/material";
+import useFundingDetailQuery from "@/query/useFundingsQuery";
+import FundingPageTab from "@/app/(without-navbar)/fundings/[fundId]/view/FundingPageTab";
+import FundingTitle from "@/app/(without-navbar)/fundings/[fundId]/view/FundingTitle";
+import FundingProgress from "@/app/(without-navbar)/fundings/[fundId]/view/FundingProgress";
+import FundingThumbnail from "@/app/(without-navbar)/fundings/[fundId]/view/FundingThumbnail";
+
+export default function FundingDetailPage({
+  params,
+}: {
+  params: { fundId: string };
+}) {
+  const { data: funding } = useFundingDetailQuery(params.fundId);
+
   return (
     <>
-      <h1>펀딩 상세 페이지</h1>
+      {funding && (
+        <Stack direction={"column"} spacing={1}>
+          <FundingThumbnail funding={funding} />
+          <Stack padding={3} spacing={2}>
+            <FundingTitle funding={funding} />
+            <FundingProgress funding={funding} />
+          </Stack>
+          <FundingPageTab funding={funding} />
+        </Stack>
+      )}
     </>
   );
 }
