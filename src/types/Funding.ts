76e253a51b9227686dfb4ae6a827d8ae859e3dc5
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2024-04-17 14:24:24 +0900

    :sparkles: Feat: 펀딩 상세 정보 조회 hook 추가

---
+++ src/types/Funding.ts
@@ -0,0 +1,13 @@
+export interface Funding {
+  fundId: number;
+  fundUuid: string;
+  fundTitle: string;
+  fundCont: string;
+  fundImg: string;
+  fundTheme: string;
+  fundPubl: boolean;
+  fundGoal: number;
+  fundSum: number;
+  endAt: string;
+  regAt: string;
+}
