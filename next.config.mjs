0f7e1ee29f1023cf90706bdd6ea937c28d55836a
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2024-04-17 14:18:41 +0900

    :wrench: Chore: API URI rewrites 설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "standalone",
+    async rewrites() {
+        return [
+            {
+                source: "/api/:path*",
+                destination: "http://43.201.66.126:3000/api/:path*",
+            },
+        ];
+    },
 };
 
 export default nextConfig;
