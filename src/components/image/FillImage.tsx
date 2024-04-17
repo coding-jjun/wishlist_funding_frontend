c7e36a6d99a354251b5b59491dcd7cde42382f75
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2024-04-17 16:39:59 +0900

    :sparkles: Feat: `FillImage` 컴포넌트 추가

---
+++ src/components/image/FillImage.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import type { CSSProperties } from "react";
+
+interface Props {
+  src: string;
+  alt: string;
+  parentDivStyle?: CSSProperties;
+}
+
+export default function FillImage({ src, alt, parentDivStyle }: Props) {
+  return (
+    <div
+      style={{
+        position: "relative",
+        overflow: "hidden",
+        objectFit: "cover",
+        ...parentDivStyle,
+      }}
+    >
+      <Image src={src} alt={alt} fill={true} />
+    </div>
+  );
+}
