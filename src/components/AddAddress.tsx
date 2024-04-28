7e13fa6cfa323675f4b902323356d12074117b37
choihyerln <choi.hyehyehihi@gmail.com>
2024-04-29 01:06:32 +0900

    ✨ Feat: 펀딩 개설 페이지 `배송지` 필드 추가

---
+++ src/components/AddAddress.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Button, TextField, IconButton } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import Postcode from "@/components/Postcode";
+
+interface Prop {
+  onClose: () => void;
+}
+
+export default function AddAddress({ onClose }: Prop) {
+  const handleClose = () => {
+    onClose();
+  };
+
+  return (
+    <>
+      <IconButton onClick={handleClose} color="primary" aria-label="close">
+        <CloseIcon />
+      </IconButton>
+      <h3>새 배송지 추가</h3>
+      <div>
+        <TextField label="배송지 명" fullWidth margin="normal" />
+        <TextField label="받으실 분" fullWidth margin="normal" />
+        <TextField label="휴대폰 번호" fullWidth margin="normal" />
+        <Postcode />
+        <Button variant="contained">확인</Button>
+      </div>
+    </>
+  );
+}
