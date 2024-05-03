b091d5b4f5189b9ab88d79202f5212a918265a2a
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2024-05-03 21:42:28 +0900

    :sparkles: Feat: 펀딩 상세 정보 조회 페이지 - 롤링페이퍼 탭 추가

---
+++ src/app/(without-navbar)/fundings/[fundId]/view/RollingPaperPanel.tsx
@@ -0,0 +1,38 @@
+import useRollingPapersQuery from "@/query/useRollingPapersQuery";
+import { Stack, Typography } from "@mui/material";
+import { ProfileBox } from "@/components/profile";
+import { MoneyChip } from "@/components/chip";
+import { SizeToggleImage } from "@/components/image";
+
+interface Props {
+  fundUuid: string;
+}
+
+export default function RollingPaperPanel({ fundUuid }: Props) {
+  const { data: rollingPapers } = useRollingPapersQuery(fundUuid);
+
+  return (
+    <>
+      {rollingPapers?.map((paper) => (
+        <ProfileBox
+          key={`rolling-paper-${paper.rollId}`}
+          userName={paper?.donation?.user?.userNick ?? "익명"}
+          regAt={paper.donation?.regAt.toString()}
+          description={<MoneyChip amount={paper.donation.donAmnt} />}
+          content={
+            <Stack direction="column" spacing={1}>
+              <Typography variant="body1">{paper.rollMsg}</Typography>
+              {paper.rollImg ?? (
+                <SizeToggleImage
+                  src={paper.rollImg}
+                  alt={"롤링페이퍼"}
+                  width={100}
+                />
+              )}
+            </Stack>
+          }
+        />
+      ))}
+    </>
+  );
+}
