8591937d580ccd879e0641906568af03cae3388c
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2024-06-23 15:11:32 +0900

    :sparkles: Feat: 프로필 이미지 아바타 및 프로필 이미지 수정용 Bottom Sheet 추가

---
+++ src/components/avatar/AvatarWithBadge.tsx
@@ -0,0 +1,34 @@
+import React, { CSSProperties, ReactNode } from "react";
+import { Avatar, Badge } from "@mui/material";
+
+interface Props {
+  imgSrc: string;
+  badge: ReactNode;
+  onClick?: () => void;
+  avatarSx?: CSSProperties;
+}
+
+export const AvatarWithBadge = ({
+  imgSrc,
+  badge,
+  onClick,
+  avatarSx,
+}: Props) => {
+  return (
+    <Badge
+      overlap="circular"
+      anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+      badgeContent={badge}
+      onClick={onClick}
+    >
+      <Avatar
+        sx={{
+          width: 100,
+          height: 100,
+          ...avatarSx,
+        }}
+        src={imgSrc}
+      />
+    </Badge>
+  );
+};
