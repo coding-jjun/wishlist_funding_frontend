7aa8da1ee558c9e1863fa9224ccace265f41c088
choihyerln <choi.hyehyehihi@gmail.com>
2024-07-25 23:41:57 +0900

    ✨ Feat: `친구요청` 수락, 무시 기능 추가

--- src/app/(without-navbar)/notification/view/IncomingFollowButtons.tsx
+++ src/app/(without-navbar)/notification/view/IncomingFollowButtons.tsx
@@ -1,6 +1,29 @@
 import { Box, Button, ButtonGroup } from "@mui/material";
+import useAddFriend from "@/query/useAddFriend";
+import useDeleteFriend from "@/query/useDeleteFriend";
 
-export default function IncomingFollowButtons() {
+interface Props {
+  sendId: number;
+}
+
+export default function IncomingFollowButtons({ sendId }: Props) {
+  // TODO: 로그인한 유저 아이디로 수정 필요
+  const userId: number = 1;
+
+  const { mutate: addFriend } = useAddFriend(userId, sendId);
+  const { mutate: deleteFriend } = useDeleteFriend(userId, sendId);
+
+  const handleSubmit = (type: string) => {
+    if (type === "add") {
+      addFriend();
+    } else if (type === "delete") {
+      deleteFriend();
+    }
+    setTimeout(() => {
+      window.location.reload();
+    }, 50);
+  };
+
   return (
     <ButtonGroup
       sx={{
@@ -14,17 +37,23 @@ export default function IncomingFollowButtons() {
         }}
       >
         <Button
+          onClick={() => handleSubmit("add")}
           sx={{
             borderColor: "#d0d0d0",
             borderRadius: 2,
             color: "#4F4635",
             padding: "3px",
             flex: 1,
+            "&:hover": {
+              borderColor: "#FFE022",
+              backgroundColor: "#FFE022",
+            },
           }}
         >
           수락
         </Button>
         <Button
+          onClick={() => handleSubmit("delete")}
           sx={{
             borderColor: "#d0d0d0",
             borderRadius: 2,
@@ -32,6 +61,10 @@ export default function IncomingFollowButtons() {
             padding: "3px",
             ml: 0.5,
             flex: 1,
+            "&:hover": {
+              borderColor: "#FFE022",
+              backgroundColor: "#FFE022",
+            },
           }}
         >
           무시
