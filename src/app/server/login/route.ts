c7aba23e6d32266ee60ff60cb6b0e9878b6c0d7f
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2024-08-25 12:05:52 +0900

    :bug: fix: 로그인 API https로 수정

--- src/app/server/login/route.ts
+++ src/app/server/login/route.ts
@@ -6,7 +6,7 @@ export async function POST(request: Request): Promise<NextResponse> {
     const body = await request.json();
 
     const response = await axios.post(
-      "http://api.giftogether.co.kr/auth/login",
+      "https://api.giftogether.co.kr/auth/login",
       body,
       {
         withCredentials: true,
