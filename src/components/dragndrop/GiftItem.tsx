efbda8e08c0f7476482b39472367692b57808e2e
choihyerln <choi.hyehyehihi@gmail.com>
2024-09-30 17:46:53 +0900

    ✨ Feat: `대표 이미지` 지정 기능 추가

--- src/components/dragndrop/GiftItem.tsx
+++ src/components/dragndrop/GiftItem.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { Box, Card, CardContent, TextField } from "@mui/material";
+import { Box, Card, CardContent, IconButton, TextField } from "@mui/material";
 import { useFormContext, useWatch } from "react-hook-form";
 import GiftDto from "@/types/GiftDto";
 import DragHandler from "@/components/dragndrop/DragHandler";
 import axios from "axios";
+import StarIcon from "@mui/icons-material/Star";
+import StarBorderIcon from "@mui/icons-material/StarBorder";
 
 interface GiftItemProps {
   id: number;
@@ -33,11 +35,13 @@ export default function GiftItem({
   const DUMMY: string = "/dummy/present.webp";
 
   const [thumbnail, setThumbnail] = useState<string | null>(null);
+  const [isPrimary, setIsPrimary] = useState<boolean>(false); // 대표 이미지 상태
 
   useEffect(() => {
     // url 지우면 기존에 있던 썸네일도 제거
     if (giftUrl === "") {
       setThumbnail(null);
+      setIsPrimary(false);
       return;
     }
 
@@ -50,7 +54,7 @@ export default function GiftItem({
               url: giftUrl,
             },
           );
-          console.log("Fetched metadata from API:", response.data);
+          console.log("Ⓜ️ 메타데이터:", response.data);
           const imageUrl = response.data.image || null;
           setThumbnail(response.data.image || DUMMY);
           setValue(`gifts[${index - 1}].giftImg`, imageUrl);
@@ -65,6 +69,12 @@ export default function GiftItem({
     }
   }, [giftUrl, setValue]);
 
+  // 대표이미지 설정하는 함수
+  const handleSetPrimary = () => {
+    setIsPrimary(!isPrimary);
+    setValue(`gifts[${index - 1}].isPrimary`, !isPrimary);
+  };
+
   return (
     <Card
       sx={{
@@ -77,18 +87,46 @@ export default function GiftItem({
         <DragHandler gifts={gifts} id={id} onDelete={onDelete} />
 
         {/*썸네일*/}
-        <Box sx={{ display: "flex", alignItems: "flex-start" }}>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "flex-start",
+            position: "relative",
+          }}
+        >
           {thumbnail && (
-            <Box
-              component="img"
-              src={thumbnail}
-              sx={{
-                width: "100%",
-                height: "auto",
-                marginTop: 2,
-                borderRadius: 2,
-              }}
-            />
+            <div>
+              <Box
+                component="img"
+                src={thumbnail}
+                sx={{
+                  width: "100%",
+                  height: "auto",
+                  marginTop: 2,
+                  borderRadius: 2,
+                }}
+              />
+
+              {/*대표 이미지 지정*/}
+              <IconButton
+                onClick={handleSetPrimary}
+                sx={{
+                  position: "absolute",
+                  top: 30,
+                  right: 10,
+                  zIndex: 10,
+                  backgroundColor: "white",
+                  borderRadius: "50%",
+                  boxShadow: "0px 2px 5px rgba(0,0,0,0.2)",
+                }}
+              >
+                {isPrimary ? (
+                  <StarIcon sx={{ color: "gold" }} />
+                ) : (
+                  <StarBorderIcon sx={{ color: "gray" }} />
+                )}
+              </IconButton>
+            </div>
           )}
         </Box>
 
